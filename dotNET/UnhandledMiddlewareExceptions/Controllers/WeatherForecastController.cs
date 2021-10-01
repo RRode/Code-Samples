bef7879014861d7ef4249f81445ae96a24c717ee
Rok Rode <RRode@users.noreply.github.com>
2021-10-01 07:54:43 +0200

    Unhandled middleware exception sample

---
+++ dotNET/UnhandledMiddlewareExceptions/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace UnhandledMiddlewareExceptions.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private WeatherForecast _forecast;
+
+        public WeatherForecastController()
+        {
+            _forecast = new WeatherForecast
+            {
+                Date = DateTime.Now,
+                Summary = "Sunny",
+                TemperatureC = 25
+            };
+        }
+
+        [HttpGet]
+        [Route("crash")]
+        public WeatherForecast GetCrashed()
+        {
+            //Intentionally not awaited to cause an issue.
+            CrashMeAsyncVoid();
+
+            return _forecast;
+        }
+
+        private async void CrashMeAsyncVoid()
+        {
+            await Task.Delay(100);
+
+            throw new Exception("Crashed by async void method.");
+        }
+
+        [HttpGet]
+        [Route("uncaught")]
+        public WeatherForecast GetUncaughtException()
+        {
+            //Intentionally not awaited to cause an issue.
+            CrashMeAsyncTask();
+
+            return _forecast;
+        }
+
+        private async Task CrashMeAsyncTask()
+        {
+            await Task.Delay(100);
+
+            throw new Exception("Crashed by async Task method.");
+        }
+    }
+}
